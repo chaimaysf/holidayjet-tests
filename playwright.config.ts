6bccd95cf86d08cd984a87b0392c28b6ba263f11
Chaima <chaimaysf2000@gmail.com>
2026-06-01 22:25:56 +0200

    fix tsconfig and process env

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,15 @@
+/// <reference types="node" />
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
   timeout: 60000,
-
   use: {
     headless: false,
     launchOptions: {
       slowMo: 500,
     },
-    baseURL: 'https://www.holidayjet.co.uk',
+    baseURL: process.env.BASE_URL || 'https://www.holidayjet.co.uk',
   },
   projects: [
     {
